8b5465633ffd918542b5bf568e6d8dacb9a7809e
Vladimir Kharchenco <82471986+BreeZ27@users.noreply.github.com>
2022-03-30 11:13:56 +0300

    Delete gen.dart

--- flutter_hw_12/packages/myGen/gen.dart
+++
@@ -1,9 +0,0 @@
-// const locPath = 'packages/myGen';
-
-// const inlet = 'DO NOT EDIT BY HANDS. This is code for homework.';
-// const outlet = 'GENERATED CODE END';
-
-// const _prologue = """class HomeClass {
-  
-//   """;
-// const _epilogue = '}';
